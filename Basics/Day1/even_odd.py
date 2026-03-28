a0c5b344a62068610daf7a39c31890db4f9a5d79
anjan <anjan850861@gmail.com>
2026-03-28 19:10:31 +0530

    Day1:Added even odd problem

---
+++ Basics/Day1/even_odd.py
@@ -0,0 +1,10 @@
+#day-1
+# check if number is even or odd
+def check_even_odd(n):
+    if n%2==0:
+        print("even")
+    else:
+        print('odd')
+n=int(input("enter number:"))  
+check_even_odd(n) 
+ 
